49161df6ed92935cdcede5280f6e51dac4d2ee4e
王冠华 <767986297@qq.com>
2020-02-21 13:38:39 +0800

    第二次通过分支提交到远程仓库

--- app/src/main/java/com/bawei/day0221/MainActivity.java
+++ app/src/main/java/com/bawei/day0221/MainActivity.java
@@ -3,6 +3,9 @@ package com.bawei.day0221;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 通过分支,写类注释提交到远程仓库
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
